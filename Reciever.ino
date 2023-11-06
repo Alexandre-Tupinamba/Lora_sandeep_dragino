8ca851f8c173cfb0d1e21faa022155fddf5490b6
Alexandre-Tupinamba <60014649+Alexandre-Tupinamba@users.noreply.github.com>
2023-11-06 14:34:37 -0400

    Create Reciever.ino

---
+++ Reciever.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+#define SS 10
+#define RST 9
+#define DIO0 2
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+  LoRa.setPins(SS, RST, DIO0);
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
